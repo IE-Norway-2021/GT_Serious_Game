335649ea0a92669803c55cd639cb8e3360152a72
David Gonzalez Leon <david.gonzalez@citycable.ch>
2022-11-14 14:15:42 +0100

    base structure ~

---
+++ Assets/Scripts/Model/Manager.cs
@@ -0,0 +1,28 @@
+// Classe qui s'occupe de la gestion de la partie
+/*
+Connait le nombre d'usines qui sont placées et de quel type
+Génère le nombre de ressources de chaque type produites dépendant des usines présentes
+Génère le nombre de ressources de chaque type consommées dépendant des usines présentes
+Génère le nombre de ressources de chaque type stockées dépendant des usines présentes
+Génère la quantité de CO2 produite dépendant des usines présentes
+Génère les prompts pour quand les activistes et les tribus arrivent
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+}
--- base.md
+++ base.md
@@ -5,3 +5,11 @@
     -   Add -> 3D Object -> Terrain
     -   ça va ouvrir un éditeur de terrain qui permet d'ajouter des montagnes,...
 -   Essayer de prendre le plus possible d'assets stores
+
+Structure :
+
+Dans le dossier scripts, deux dossiers :
+
+-   un dossier modèle, représente le modèle logique de l'application (aucune interface graphique)
+-   un dossier pour toute la partie graphique/liée à l'affichage (classes qui héritent de MonoBehaviour)
+-   Quelques classes qui font le liens entre les deux
