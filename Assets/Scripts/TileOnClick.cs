db3ea7874ab81d861503f1882d9c1b2783f90f41
Satche <info@satche.ch>
2022-11-24 15:44:01 +0100

    Fix bad merge

--- Assets/Scripts/TileOnClick.cs
+++ Assets/Scripts/TileOnClick.cs
@@ -8,7 +8,7 @@ public class TileOnClick : MonoBehaviour
     // // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // // Update is called once per frame
@@ -16,18 +16,27 @@ public class TileOnClick : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
             RaycastHit hit;
-
+            
             if (Physics.Raycast(ray, out hit, 100))
             {
-                // toggleOutline();
-                if (hit.transform.gameObject.GetComponent<Outline>() != null)
-                {
-                    GameObject tile = hit.transform.gameObject;
-                    toggleOutline(tile);
-                }
+                GameObject tile = hit.transform.gameObject;
+                toggleOutline(tile);
             }
         }
+    }
+
+    void toggleOutline(GameObject gameobject)
+    {
+        // Disable the outline for all tiles
+        GameObject[] tiles = GameObject.FindGameObjectsWithTag("Tile");
+        foreach (GameObject tile in tiles)
+        {
+            tile.GetComponent<Outline>().enabled = false;
+        }
+
+        // Enable the outline for the clicked tile
+        gameobject.GetComponent<Outline>().enabled = true;
     }
 }
