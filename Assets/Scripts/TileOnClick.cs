6e9c9ba44f38ed410753f3fa956b05c8d5797848
Satche <info@satche.ch>
2022-12-03 18:53:49 +0100

    Add feature tile removing with UI button

--- Assets/Scripts/TileOnClick.cs
+++ Assets/Scripts/TileOnClick.cs
@@ -10,23 +10,31 @@ public class TileOnClick : MonoBehaviour
     public GameObject selectedTile;
     public Action<GameObject> OnTileClick;
 
-    // // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            // Avoid ray casting on UI
+            if (EventSystem.current.IsPointerOverGameObject()) return;
+
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
 
             if (Physics.Raycast(ray, out hit, 100))
             {
                 GameObject tile = hit.transform.gameObject;
+
+                // TODO: list all the clickable tiles
+                if (tile.tag == "Tile")
+                {
+                    selectedTile = tile;
+                    OnTileClick?.Invoke(tile);
+                } else {
+                    selectedTile = null;
+                    OnTileClick?.Invoke(null);
+                }
                 OnTileClick?.Invoke(tile);
             }
         }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,40 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    public TileOnClick tileOnClick;
 
+	// Buttons
+	public Button selectedButton;
+    public Button removeButton;
+
+	// Progress bars
 	public ProgressBar money;
 	public float newMoney;
-    public TMP_Text moneyText;
-    public ProgressBar co2;
-    public float newCo2;
-    public TMP_Text co2Text;
+	public TMP_Text moneyText;
+	public ProgressBar co2;
+	public float newCo2;
+	public TMP_Text co2Text;
 
 
 	// Start is called before the first frame update
 	void Start()
 	{
-        // Update resource every 1 second
-        InvokeRepeating("UpdateResource", 0, 1);
+        tileOnClick = GameObject.Find("TileOnClickHandler").GetComponent<TileOnClick>();
+        removeButton = GameObject.Find("RemoveButton").GetComponent<Button>();
+		// Update resource every 1 second
+		InvokeRepeating("UpdateResource", 0, 1);
 	}
 
-	void UpdateResource()
+	void Update()
 	{
-        // Money
-        money = GameObject.Find("MoneyProgressBar").GetComponent<ProgressBar>();
-        moneyText = GameObject.Find("MoneyCount").GetComponent<TMP_Text>();
+        // Enable button according to selected tile/object
+        if (tileOnClick.selectedTile != null)
+        {
+            removeButton.interactable = true;
+        }
+        else
+        {
+            removeButton.interactable = false;
+        }
+	}
+
+	private void UpdateResource()
+	{
+		// Money
+		money = GameObject.Find("MoneyProgressBar").GetComponent<ProgressBar>();
+		moneyText = GameObject.Find("MoneyCount").GetComponent<TMP_Text>();
 		newMoney = GameObject.Find("GameManager").GetComponent<GameManager>().moneyCount;
 		money.current = newMoney;
-        moneyText.text = newMoney.ToString();
+		moneyText.text = newMoney.ToString();
 
-        // CO2
-        co2 = GameObject.Find("CO2ProgressBar").GetComponent<ProgressBar>();
-        co2Text = GameObject.Find("CO2Count").GetComponent<TMP_Text>();
-        newCo2 = GameObject.Find("GameManager").GetComponent<GameManager>().co2Count;
-        co2.current = newCo2;
-        co2Text.text = newCo2.ToString();
+		// CO2
+		co2 = GameObject.Find("CO2ProgressBar").GetComponent<ProgressBar>();
+		co2Text = GameObject.Find("CO2Count").GetComponent<TMP_Text>();
+		newCo2 = GameObject.Find("GameManager").GetComponent<GameManager>().co2Count;
+		co2.current = newCo2;
+		co2Text.text = newCo2.ToString();
+	}
+
+	public void ButtonClicked()
+	{
+		// Log current clicked button
+		selectedButton = EventSystem.current.currentSelectedGameObject.GetComponent<Button>();
+        if (selectedButton.name == "RemoveButton")
+        {
+            Destroy(tileOnClick.selectedTile.gameObject);
+        }
+        tileOnClick.selectedTile = null;
 	}
 }
